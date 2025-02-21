91dd7fe9c36b4921ede3f850d6243f1f1ae17a6a
ulmsaga <sclee1115@gmail.com>
2025-02-21 20:44:09 +0900

    yaml make test

--- accounts/accounts-deploy.sh
+++ accounts/accounts-deploy.sh
@@ -12,11 +12,21 @@ docker build --platform=linux/amd64 -t k8s-vga-worker1:5000/${TEAM_PREFIX}-${APP
 echo "docker push k8s-vga-worker1:5000/${TEAM_PREFIX}-${APP_NAME}:v${VERSION}"
 docker push k8s-vga-worker1:5000/${TEAM_PREFIX}-${APP_NAME}:v${VERSION}
 
-# Replace variables in k8s yaml template and apply
+# Backup existing k8s yaml file if it exists
+if [ -f accounts-k8s.yaml ]; then
+    timestamp=$(date +%Y%m%d_%H%M%S)
+    cp accounts-k8s.yaml "accounts-k8s.yaml.backup_${timestamp}"
+fi
+
+# Generate new k8s yaml file from template
+echo "Generating new accounts-k8s.yaml from template..."
 cat accounts-k8s-template.yaml | \
     sed "s/\${NAMESPACE}/${NAMESPACE}/g" | \
     sed "s/\${TEAM_PREFIX}/${TEAM_PREFIX}/g" | \
     sed "s/\${APP_NAME}/${APP_NAME}/g" | \
     sed "s/\${VERSION}/${VERSION}/g" | \
-    sed "s/\${NODE_PORT}/${NODE_PORT}/g" | \
-    kubectl apply -f -
+    sed "s/\${NODE_PORT}/${NODE_PORT}/g" > accounts-k8s.yaml
+
+# Apply the generated yaml file
+echo "Applying generated accounts-k8s.yaml..."
+kubectl apply -f accounts-k8s.yaml
